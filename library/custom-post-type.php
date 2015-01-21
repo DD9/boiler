9059e6168badf5ab8946693152120fa5d0ae3ff8
Todd Dumas <todd@dd9.com>
2015-01-21 14:41:07 -0700

    update CPT template, going to try to push to superproject and subtree

--- library/custom-post-type.php
+++ library/custom-post-type.php
@@ -1,18 +1,14 @@
 <?php
-/* Bones Custom Post Type Example
-This page walks you through creating 
-a custom post type and taxonomies. You
-can edit this one or copy the following code 
-to create another one. 
+/* Custom Post Types
 
-I put this in a separate file so as to 
-keep it organized. I find it easier to edit
-and change things if they are concentrated
-in their own file.
-
-Developed by: Eddie Machado
-URL: http://themble.com/bones/
 */
+
+//DD9 Notice:
+//CPT DISABLED IN Boiler by default
+//Hardcode custom post types here if you want to keep them out of the dashboard
+//Otherwise use
+//https://wordpress.org/plugins/custom-post-type-ui/
+
 
 // Flush rewrite rules for custom post types
 add_action( 'after_switch_theme', 'bones_flush_rewrite_rules' );
@@ -22,9 +18,7 @@ function bones_flush_rewrite_rules() {
 	flush_rewrite_rules();
 }
 
-//DISBLED IN Boiler
-//Hard Code custom post types here if we want to keep them out of the dashboard
-//https://wordpress.org/plugins/custom-post-type-ui/
+
 
 
 ?>
