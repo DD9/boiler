9661bb05994e24cb292864f1056caf513742cf0f
Hilary <hilary@dd9.com>
2015-07-14 15:30:28 -0600

    clean up footer.php and footer.less

--- footer.php
+++ footer.php
@@ -9,18 +9,14 @@ $googleplus = get_field('googleplus_url', 'option');
 ?>
 
 
-<footer id="footer" class="clearfix">
-  <div id="footer-widgets">
-
+<footer id="footer">
+  <div id="footer-upper">
     <div class="container">
-
-    <div id="footer-wrapper">
-
       <div class="row">
+      
         <div class="col-sm-6 col-md-3">
           <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-1') ) : ?>
           <?php endif; ?>
-          
           
           <?php // ACF global options: Contact Info ?>
           <ul class="footer-contact">
@@ -55,46 +51,38 @@ $googleplus = get_field('googleplus_url', 'option');
               <li class="googleplus"><a href="<?= $googleplus ?>" target="_blank"><i class="fa fa-google-plus"></i></a></li>
             <?php } ?>
           </ul>
-          
-          
-        </div> <!-- end widget1 -->
+        </div> <!-- /col -->
 
         <div class="col-sm-6 col-md-3">
           <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-2') ) : ?>
           <?php endif; ?>
-        </div> <!-- end widget1 -->
+        </div> <!-- /col -->
 
         <div class="col-sm-6 col-md-3">
           <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-3') ) : ?>
           <?php endif; ?>
-        </div> <!-- end widget1 -->
+        </div> <!-- /col -->
 
         <div class="col-sm-6 col-md-3">
           <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-4') ) : ?>
           <?php endif; ?>
-        </div> <!-- end widget1 -->
+        </div> <!-- /col -->
 
-      </div> <!-- end .row -->
-
-    </div> <!-- end #footer-wrapper -->
-
-    </div> <!-- end .container -->
-  </div> <!-- end #footer-widgets -->
+      </div> <!-- /row -->
+    </div><!-- /container -->
+  </div> <!-- /footer-upper -->
 
   <div id="sub-floor">
     <div class="container">
       <div class="row">
-        <div class="col-md-4 copyright">
-          &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>  <a class="site-credit" title="Website Design and Development by DD9.com" target="_blank" href="http://dd9.com">Website by DD9</a>
-        </div>
-        <div class="col-md-4 col-md-offset-4 attribution">
-          Boiler theme by <a target="_blank" href="http://dd9.com">DD9</a> - <a href="/assets">Assets</a> - <a href="/wp-admin">Admin Login</a>
-        </div>
-      </div> <!-- end .row -->
-    </div>
-  </div>
+        <div class="col-md-12">
+          Boiler theme by <a target="_blank" href="http://dd9.com">DD9</a> - <a href="/assets">Assets</a> - <a href="/wp-admin">Admin Login</a> -  &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>  <a class="site-credit" title="Website Design and Development by DD9.com" target="_blank" href="http://dd9.com">Website by DD9</a>
+        </div> <!-- /col -->
+      </div> <!-- /row -->
+    </div> <!-- /container -->
+  </div> <!-- /sub-floor -->
 
-</footer> <!-- end footer -->
+</footer> <!-- /footer -->
 
 <?php  #all js scripts are loaded in library/bones.php ?>
 <?php wp_footer(); ?>
