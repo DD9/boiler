4cfbdcc35ef7409f0a76982fb863f22edca4da62
Todd Dumas <todd@dd9.com>
2017-03-10 14:13:16 -0700

    restructure acf json

--- functions.php
+++ functions.php
@@ -58,11 +58,11 @@ if( function_exists('acf_add_options_page') ) {
 }
 
  //https://www.advancedcustomfields.com/resources/local-json/
-function my_acf_json_save_point( $path ) {
-  $path = get_stylesheet_directory() . '/acf';
-  return $path;
-}
-add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+// function my_acf_json_save_point( $path ) {
+//   $path = get_stylesheet_directory() . '/acf-json';
+//   return $path;
+// }
+// add_filter('acf/settings/save_json', 'my_acf_json_save_point');
 
 
 /* library/bones.php (functions specific to BREW)
