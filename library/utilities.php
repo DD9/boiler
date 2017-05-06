8d078cc45c72bf1f3f5575c9207c17a623e76592
Todd Dumas <todd@dd9.com>
2017-05-06 15:09:12 -0600

    human readable array utility function, untested

--- library/utilities.php
+++ library/utilities.php
@@ -16,3 +16,19 @@ function pr($input = null, $exit = false, $classes = null){
 	if ($exit) exit;
 }
 
+
+/**
+ * Creates a human readable list of an array
+ * https://gist.github.com/SleeplessByte/4514697
+ * 
+ * @param string[] $ranges array to list items of
+ * @param string $glue normal glue between items
+ * @param string $last glue between last two items
+ *
+ * @remarks works with 0, 1, 2 or 3+ items
+ * @returns string 'item1, item2, item3 or item4'
+ */
+function array_listing( $ranges, $glue = ', ', $last = ' or ') {
+  array_splice( $ranges, -2, 2, implode( $last, array_slice( $ranges, -2, 2 ) ) );
+  return implode( $glue , $ranges );
+}
