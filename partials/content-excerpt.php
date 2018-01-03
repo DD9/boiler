e2dcccce2e43c60add205aee978808dca168a77d
Hilary Clarcq <hilary@dd9.com>
2018-01-03 12:06:40 -0700

    update to excerpts

--- partials/content-excerpt.php
+++ partials/content-excerpt.php
@@ -1,8 +1,8 @@
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
 
-  <header class="entry-excerpt-header">
-    <h3 class="entry-excerpt-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+  <header class="excerpt-header">
+    <h3 class="excerpt-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
     <p class="byline vcard">
       by <span class="author"><em><?php echo bones_get_the_author_posts_link() ?></em></span> - 
       <time class="updated" datetime="<?php get_the_time('Y-m-j') ?>"><?php echo get_the_time(get_option('date_format')) ?></time>
@@ -10,11 +10,11 @@
   </header>
   
   <?php if ($image) { ?>
-  <section class="featured-content featured-img featured-img-bg" style="background-image: url('<?php echo $image[0]; ?>')">
+  <section class="excerpt-img excerpt-img-bg" style="background-image: url('<?php echo $image[0]; ?>')">
   </section>
   <?php } ?>
 
-  <section class="entry-excerpt clearfix">
+  <section class="excerpt-content clearfix">
     <?php the_excerpt('<span class="read-more">' . __("Read more on","bonestheme") . ' "'.the_title('', '', false).'" &raquo;</span>'); ?>
     <?php wp_link_pages(
       array(
@@ -24,10 +24,10 @@
     ); ?>
   </section>
 
-  <footer class="article-footer clearfix">
+  <footer class="excerpt-footer clearfix">
     <span class="tags pull-left"><?php printf( '<span class="">' . __( 'in %1$s&nbsp;&nbsp;', 'bonestheme' ) . '</span>', get_the_category_list(', ') ); ?> <?php the_tags( '<span class="tags-title">' . __( '<i class="fa fa-tags"></i>', 'bonestheme' ) . '</span> ', ', ', '' ); ?></span>
     <span class="commentnum pull-right"><a href="<?php comments_link(); ?>"><?php comments_number( '<i class="fa fa-comment"></i> 0', '<i class="fa fa-comment"></i> 1', '<i class="fa fa-comment"></i> %' ); ?></a></span>
-  </footer> <?php // end article footer ?>
+  </footer> <?php // end excerpt footer ?>
 
   <?php // comments_template(); // uncomment if you want to use them ?>
 
